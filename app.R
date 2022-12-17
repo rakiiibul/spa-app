c495bfdbcf9e23a57c799707b11cd008796b6b01
rakiiibul <58264212+rakiiibul@users.noreply.github.com>
2022-12-17 15:35:36 +0100

    added new data set and visulization
    added new data set and visulization

--- app.R
+++ app.R
@@ -6,45 +6,67 @@
 #
 #    http://shiny.rstudio.com/
 #
+
 install.packages("shiny")
+install.packages("readr")
+install.packages("maps")
+install.packages("mapproj")
+library(readr)
 library(shiny)
+library(maps)
+library(mapproj)
+
+counties <- readRDS("data/counties.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  titlePanel("censasdasdusVis"),
 
-    # Application title
-    titlePanel("Customer Personality Analysis"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+sidebarLayout(
+  sidebarPanel(
+    helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+    
+    selectInput("var", 
+                label = "Choose a variable to display",
+                choices = c("Percent White", "Percent Black",
+                            "Percent Hispanic", "Percent Asian"),
+                selected = "Percent White"),
+    
+    sliderInput("range", 
+                label = "Range of interest:",
+                min = 0, max = 100, value = c(0, 100))
+  ),
+  
+  mainPanel(plotOutput("map"))
 )
+)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
+    
+    color <- switch(input$var, 
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+    
+    legend <- switch(input$var, 
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+    percent_map(data, color, legend, input$range[1], input$range[2])
+  })
 }
 
 # Run the application 
